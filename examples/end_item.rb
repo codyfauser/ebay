d49c333aab50f30937fc1cc599258ba1f210eb10
Cody Fauser <codyfauser@gmail.com>
2007-05-31 18:47:14 +0000

    Update the end item example to include the ending reason

--- examples/end_item.rb
+++ examples/end_item.rb
@@ -5,11 +5,13 @@ $:.unshift File.join(File.dirname(__FILE__),'..', 'lib')
 require 'ebay'
 require 'config'
 
+include Ebay::Types
+
 ebay = Ebay::Api.new
 
 begin
   # End the item as passed in as the first argument
-  response = ebay.end_item(:item_id => ARGV.first)
+  response = ebay.end_item(:item_id => ARGV.first, :ending_reason => EndReasonCode::LostOrBroken)
   puts "Successfully ended item #{ARGV.first}"
 rescue Ebay::RequestError => e
   e.errors.each do |error|
