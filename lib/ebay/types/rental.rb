2382a53eca6d170c50fd27e62dc7b82d0491a03f
Jose Tenorio Cesar Costa <JoseTenorio@Jose-Tenorio-Cesar-Costas-MacBook-Pro.local>
2011-05-09 12:08:18 -0300

    Changes to accept the new AddItem Request format

--- lib/ebay/types/rental.rb
+++ lib/ebay/types/rental.rb
@@ -1,26 +1,22 @@
-require 'ebay/types/rental_price'
-
 module Ebay # :nodoc:
   module Types # :nodoc:
     # == Rental
-    #  rental_price_node :rental_price__days_30_rental_price, "RentalPrice[@duration='Days30RentalPrice'], :optional => false"
-    #  rental_price_node :rental_price__days_45_rental_price, "RentalPrice[@duration='Days45RentalPrice'], :optional => false"
-    #  rental_price_node :rental_price__days_60_rental_price, "RentalPrice[@duration='Days60RentalPrice'], :optional => false"
-    #  rental_price_node :rental_price__days_90_rental_price, "RentalPrice[@duration='Days90RentalPrice'], :optional => false"
-    #  rental_price_node :rental_price__days_125_rental_price, "RentalPrice[@duration='Days125RentalPrice'], :optional => false"
+    #  rental_price_value_node :rental_price_value_days_30, "RentalPriceValue[@duration='Days30']", :optional => false
+    #  rental_price_value_node :rental_price_value_days_45, "RentalPriceValue[@duration='Days45']", :optional => false
+    #  rental_price_value_node :rental_price_value_days_60, "RentalPriceValue[@duration='Days60']", :optional => false
+    #  rental_price_value_node :rental_price_value_days_90, "RentalPriceValue[@duration='Days90']", :optional => false
+    #  rental_price_value_node :rental_price_value_days_125, "RentalPriceValue[@duration='Days125']", :optional => false
     #  numeric_node :item_buyout_price, 'ItemBuyoutPrice', :optional => false
     class Rental
       include XML::Mapping
       include Initializer
       root_element_name 'Rental'
-      rental_price_node :rental_price__days_30_rental_price, "RentalPrice[@duration='Days30RentalPrice']", :optional => false
-      rental_price_node :rental_price__days_45_rental_price, "RentalPrice[@duration='Days45RentalPrice']", :optional => false
-      rental_price_node :rental_price__days_60_rental_price, "RentalPrice[@duration='Days60RentalPrice']", :optional => false
-      rental_price_node :rental_price__days_90_rental_price, "RentalPrice[@duration='Days90RentalPrice']", :optional => false
-      rental_price_node :rental_price__days_125_rental_price, "RentalPrice[@duration='Days125RentalPrice']", :optional => false
+      rental_price_value_node :rental_price_value_days_30, "RentalPriceValue[@duration='Days30']", :optional => false
+      rental_price_value_node :rental_price_value_days_45, "RentalPriceValue[@duration='Days45']", :optional => false
+      rental_price_value_node :rental_price_value_days_60, "RentalPriceValue[@duration='Days60']", :optional => false
+      rental_price_value_node :rental_price_value_days_90, "RentalPriceValue[@duration='Days90']", :optional => false
+      rental_price_value_node :rental_price_value_days_125, "RentalPriceValue[@duration='Days125']", :optional => false
       numeric_node :item_buyout_price, 'ItemBuyoutPrice', :optional => false
     end
   end
 end
-
-
--- lib/support/xml_mapping/rental_price_value_node.rb
+++ lib/support/xml_mapping/rental_price_value_node.rb
@@ -1,7 +1,7 @@
 require 'xml/mapping/base'
-require "ebay/rental_price_information"
+require 'ebay/types/rental_price_value'
 
-class RentalPriceNode < XML::Mapping::SingleAttributeNode
+class RentalPriceValueNode < XML::Mapping::SingleAttributeNode
   def initialize_impl(path)
     @val_path = XML::XXPath.new(path + "/")
     @duration_path = XML::XXPath.new(path + "/@duration")
@@ -11,11 +11,11 @@ class RentalPriceNode < XML::Mapping::SingleAttributeNode
     val, duration = default_when_xpath_err { [@val_path.first(xml).text,
                                               @duration_path.first(xml).text]
     }
-    Ebay::RentalPriceInformation.new(val, duration)
+    Ebay::Types::RentalPriceValue.new(val, duration)
   end
 
   def set_attr_value(xml, value)
-    raise "Not a RentalPriceInformation object: #{value}" unless Ebay::RentalPriceInformation === value
+    raise "Not a RentalPriceValue object: #{value}" unless Ebay::Types::RentalPriceValue === value
     @val_path.first(xml, :ensure_created => true).text = value.val
     @duration_path.first(xml, :ensure_created => true).text = value.duration
   end
