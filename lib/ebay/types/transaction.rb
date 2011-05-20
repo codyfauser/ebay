472e7142a0f604278125dee321453f4b217cb235
Jose Tenorio Cesar Costa <JoseTenorio@Jose-Tenorio-Cesar-Costas-MacBook-Pro.local>
2011-05-20 17:26:59 -0300

    Added RentalTransactionInfo node into Transaction node

--- lib/ebay/types/transaction.rb
+++ lib/ebay/types/transaction.rb
@@ -10,6 +10,7 @@ require 'ebay/types/order'
 require 'ebay/types/listing_checkout_redirect_preference'
 require 'ebay/types/refund'
 require 'ebay/types/variation'
+require 'ebay/types/rental_transaction_info'
 
 module Ebay # :nodoc:
   module Types # :nodoc:
@@ -55,6 +56,7 @@ module Ebay # :nodoc:
     #  money_node :buyer_guarantee_price, 'BuyerGuaranteePrice', :optional => true
     #  object_node :variation, 'Variation', :class => Variation, :optional => true
     #  text_node :buyer_checkout_message, 'BuyerCheckoutMessage', :optional => true
+    #  array_node :rental_transaction_infos, 'RentalTransactionInfoArray', 'RentalTransactionInfo', :class => RentalTransactionInfo, :default_value => []
     class Transaction
       include XML::Mapping
       include Initializer
@@ -100,6 +102,7 @@ module Ebay # :nodoc:
       money_node :buyer_guarantee_price, 'BuyerGuaranteePrice', :optional => true
       object_node :variation, 'Variation', :class => Variation, :optional => true
       text_node :buyer_checkout_message, 'BuyerCheckoutMessage', :optional => true
+      array_node :rental_transaction_infos, 'RentalTransactionInfoArray', 'RentalTransactionInfo', :class => RentalTransactionInfo, :default_value => []
     end
   end
 end
