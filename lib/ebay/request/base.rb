c55b5aef15e6594544c780db80d6c443aec09635
Anton Pershakov <main24.fc@gmail.com>
2014-03-03 14:45:19 +0600

    Move building requester_credentials to Abstract request class

--- lib/ebay/request/base.rb
+++ lib/ebay/request/base.rb
@@ -2,18 +2,12 @@ require 'ebay/types/xml_requester_credentials'
 
 module Ebay
   module Requests
-    class Base      
+    class Base
       include Ebay::Types
       attr_accessor :auth_token, :username, :password
 
       def call_name
         self.class.to_s.split('::').last.gsub(/Request$/, '')
-      end
-
-      def requester_credentials
-        if auth_token || username || password
-          XMLRequesterCredentials.new(:ebay_auth_token => auth_token, :username => username, :password => password)
-        end
       end
     end
   end
--- lib/ebay/requests/abstract.rb
+++ lib/ebay/requests/abstract.rb
@@ -30,18 +30,25 @@ module Ebay # :nodoc:
       text_node :output_selector, 'OutputSelector', :optional => true
       text_node :warning_level, 'WarningLevel', :optional => true
       object_node :bot_block, 'BotBlock', :class => BotBlockRequest, :optional => true
+
       # eBay specifies the detail level as a collection.  The usual case is to use
-      # only a single detail level, so it is more appropriate to add an accessor for 
+      # only a single detail level, so it is more appropriate to add an accessor for
       # the normal case.
       # Reads the first detail level from the detail_levels Array.
       def detail_level
         @detail_levels.first
       end
-      
-      # Overwrites the details_levels Array with a new Array containing only the 
+
+      # Overwrites the details_levels Array with a new Array containing only the
       # value passed in as an argument
       def detail_level=(value)
         @detail_levels = Array(value)
+      end
+
+      def requester_credentials
+        if auth_token || username || password
+          XMLRequesterCredentials.new(:ebay_auth_token => auth_token, :username => username, :password => password)
+        end
       end
     end
   end
